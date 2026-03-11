82e186b137ddc8679ab1e017d12d32ce1ee52fa1
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-03-11 07:47:12 +0100

    Allat.py letrehpozasa

---
+++ main.py
@@ -0,0 +1,10 @@
+from allat import Allat
+
+# Állat 1: Bodri, faj: kutya, életkor: 5 év
+# Állat 2: Cirmi, faj: macska, életkor: 3 év
+
+allat1 = Allat("Bodri", "kutya", 5, "kert", "közepes")
+allat2 = Allat("Cirmi", "macska", 2, "ház", "közepes")
+
+print(allat1)
+print(allat2)
