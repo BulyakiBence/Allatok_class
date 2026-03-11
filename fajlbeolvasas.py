befa0bf98d40f9dbe348d4e20c14151a49a481e6
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-03-11 09:39:03 +0100

    fajlbeolvasas

--- fajlbeolvasas.py
+++ fajlbeolvasas.py
@@ -1,2 +1,21 @@
 from emlos import Macska, Kutya
-from allat import Madar, Keteltu, Hullo
+from allat import Madar, Keteltu, Hullo, Allat
+allatok = []
+with open('adatok/allatok.txt', 'r', encoding='utf-8') as forrasfajl:
+    next(forrasfajl)
+    for sor in forrasfajl:
+        nev, faj, eletkor, szorzet_szine = sor.strip().split(',')
+        
+        if faj == "kutya":
+            allatok.append(Kutya(nev, int(eletkor), "udvar", szorzet_szine))
+        elif faj == "macska":
+            allatok.append(Macska(nev, int(eletkor), "ház", szorzet_szine))
+        elif faj == "madar":
+            allatok.append(Madar(nev, int(eletkor)))     
+        elif faj == "keteltu":
+            allatok.append(Keteltu(nev, int(eletkor)))    
+        elif faj == "hullo":
+            allatok.append(Hullo(nev, int(eletkor)))
+
+for allat in allatok:
+    print(allat)
